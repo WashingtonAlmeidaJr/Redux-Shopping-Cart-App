874f1b1ccc831f1f3d127b818a12e2fe60ca48a9
Washington <washingtonjaaj@gmail.com>
2022-12-30 19:20:18 -0300

    link the project on firebase datastore using useEffect and fetch.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://redux-http-4f261-default-rtdb.firebaseio.com/cartItems.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
+
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   console.log(isLoggedIn);
   return (
